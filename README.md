d55f24e97282ef3c9e32a97389f5a5277026eb84
Nelson Neto <nelson.ash@outlook.com>
2024-09-13 12:24:31 -0300

    alterar displayname
    Alteracao de Readme
    
    Alteracao de Readme
    
    Inclusao de Rebase in README

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # prowler
 teste de ambiente angular
-
+### Dotnet
+```sh
 dotnet new .gitignore
 mkdir src
 mkdir tests
 cd tests
 dotnet new xunit -n Prowler.IntegrationTests
-
-
+```
+### GIT
+```sh
 git status
 git add .
-git commit -m "Commit inicial"
+git commit -m "Commit inicial"  
 git push
+```
+git rebase -i HEAD~3
--- tests/Prowler.IntegrationTests/UnitTest1.cs
+++ tests/Prowler.IntegrationTests/UnitTest1.cs
@@ -6,16 +6,16 @@ namespace Prowler.IntegrationTests;
 
 public class UnitTest1
 {
-// criar uma lista com mock sem a propriedade de data.
-// realizar a alteração que dirá que os 2 valores de data, serão a maior data entre eles.
-// realizar o assert entre os 2 elementos excluindo a data
-// verificar se as 2 datas são iguais a maior data
+    // criar uma lista com mock sem a propriedade de data.
+    // realizar a alteração que dirá que os 2 valores de data, serão a maior data entre eles.
+    // realizar o assert entre os 2 elementos excluindo a data
+    // verificar se as 2 datas são iguais a maior data
 
 
     private readonly IFixture _fixture = new Fixture().Customize(new AutoMoqCustomization { ConfigureMembers = true });
-    [Fact]
-[Trait("Category","Unit Test"]
-[DisplayName("Validar Lista de Entidade")]
+
+    [Trait("Category", "Unit Test")]
+    [Fact(DisplayName = "Validar Lista de Entidade")]
     public void TestSetupGet()
     {
         _fixture.Freeze<Mock<Transferencia>>()
@@ -23,15 +23,15 @@ public class UnitTest1
                 .Verifiable();
         // var transferenciaMock = new Mock<Transferencia>();
         // transferenciaMock.Object.AlteraIdade(It.IsAny<int>());
-        
+
         var movimentacao = _fixture.Create<Movimentacao>();
-        
+
         movimentacao.Transferencia.Idade.Should().Be(1);
     }
 }
 public class Transferencia
 {
-    public int Idade {get; private set;}   
+    public int Idade { get; private set; }
     public void AlteraIdade(int idade)
     {
         Idade = idade;
@@ -43,17 +43,17 @@ public class Movimentacao
     {
         Transferencia = transferencia;
     }
-    public Transferencia Transferencia {get; private set;}   
+    public Transferencia Transferencia { get; private set; }
 }
 
 public record ReceptCreatecommand(Recept Recept);
-    public record Recept(int Id)
+public record Recept(int Id)
+{
+    public ReceptCreatecommand ToCommand()
     {
-        public ReceptCreatecommand ToCommand()
-        {
-            throw new NotImplementedException();
-        }
-    };
+        throw new NotImplementedException();
+    }
+};
 // public class Handler
 // {
 //     private readonly IService _service;
